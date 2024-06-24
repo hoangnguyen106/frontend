9542e82f17264fdd3be0424f8a39d44c95ef2ac0
hoangnguyen106 <nguyenhoangnhom4@gmail.com>
2024-06-24 16:55:20 +0700

    complete-FE

--- src/app/modules/shared/utils/url.ts
+++ src/app/modules/shared/utils/url.ts
@@ -1,7 +1,5 @@
-export const nhanvienUrl =
-  'https://mass-tan-noi-2.onrender.com/api/v1/employee';
+export const nhanvienUrl = 'https://www.massage.name.vn/api/v1/employee';
 export const nhanvienRamdomUrl =
-  'https://mass-tan-noi-2.onrender.com/api/v1/employee-random';
-export const nhanvienTime =
-  'https://mass-tan-noi-2.onrender.com/api/v1/employee-time';
-export const orderUrl = 'https://mass-tan-noi-2.onrender.com/api/v1/customer';
+  'https://www.massage.name.vn/api/v1/employee-random';
+export const nhanvienTime = 'https://www.massage.name.vn/api/v1/employee-time';
+export const orderUrl = 'https://www.massage.name.vn/api/v1/customer';
